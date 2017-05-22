2f52b7f6635c7e2909106add32fc9731516a37d9
javed.khan <javedak09@gmail.com>
2017-05-22 12:11:26 +0500

    Initial Commit

---
+++ app/src/main/java/com/javedak09/zscore/MainActivity.java
@@ -0,0 +1,57 @@
+package com.javedak09.zscore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void CalcZScore(View view) {
+        DownloadData();
+    }
+
+    private void DownloadData() {
+        String str = "";
+        String str1 = "";
+        String str2 = "";
+        String str3 = "";
+        String str4 = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
